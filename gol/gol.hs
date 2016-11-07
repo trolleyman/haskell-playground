3e6593f478797be2e812833c7c1960b4635ebbd8
Callum Tolley <cgtrolley@gmail.com>
2016-11-07 23:37:39 +0000

    Added presets to GoL

--- gol/gol.hs
+++ gol/gol.hs
@@ -1,4 +1,4 @@
-import Data.List (splitAt)
+import Data.List (intersperse, splitAt)
 import Data.List.Split (splitOn)
 import Text.Read (readMaybe)
 import Data.Char (isSpace)
@@ -61,14 +61,134 @@ parseBoard :: [String] -> Board
 parseBoard = map parseLine
 
 defaultBoard :: Board
-defaultBoard = parseBoard s
-    where s = ["                ",
-               "    O           ",
-               "  O O           ",
-               "   OO           ",
-               "                ",
-               "                ",
-               "                "]
+defaultBoard = load "glider"
+
+presets :: [(String, String)]
+presets = [("blinker", "A simple oscillator of period 2"),
+           ("toad", "Another simple oscillator of period 2"),
+           ("beacon", "Yet another simple oscillator of period 2"),
+           ("pulsar", "A large oscillator of period 3"),
+           ("pentadecathlon", "A medium sized oscillator of period 15"),
+           ("glider", "A small spaceship that travels diagonally"),
+           ("lwss", "Lightweight spaceship that travels horizontally"),
+           ("r-pent", "A small seed that grows to live for a long time")]
+
+-- Example interesting Game of Life boards
+load :: String -> Board
+load s = maybe (error "load: Invalid board ID") id $ loadMaybe s
+
+loadMaybe :: String -> Maybe Board
+loadMaybe "blinker" = Just $ parseBoard s
+    where s = ["     ",
+               "  O  ",
+               "  O  ",
+               "  O  ",
+               "     "]
+
+loadMaybe "toad" = Just $ parseBoard s
+    where s = ["      ",
+               "      ",
+               "  OOO ",
+               " OOO  ",
+               "      ",
+               "      "]
+
+loadMaybe "beacon" = Just $ parseBoard s
+    where s = ["      ",
+               " OO   ",
+               " OO   ",
+               "   OO ",
+               "   OO ",
+               "      "]
+
+-- Period 3
+loadMaybe "pulsar" = Just $ parseBoard s
+    where s = ["                 ",
+               "                 ",
+               "    OOO   OOO    ",
+               "                 ",
+               "  O    O O    O  ",
+               "  O    O O    O  ",
+               "  O    O O    O  ",
+               "    OOO   OOO    ",
+               "                 ",
+               "    OOO   OOO    ",
+               "  O    O O    O  ",
+               "  O    O O    O  ",
+               "  O    O O    O  ",
+               "                 ",
+               "    OOO   OOO    ",
+               "                 ",
+               "                 "]
+
+-- Period 15
+loadMaybe "pentadecathlon" = Just $ parseBoard s
+    where s = ["           ",
+               "           ",
+               "           ",
+               "           ",
+               "           ",
+               "    OOO    ",
+               "    O O    ",
+               "    OOO    ",
+               "    OOO    ",
+               "    OOO    ",
+               "    OOO    ",
+               "    O O    ",
+               "    OOO    ",
+               "           ",
+               "           ",
+               "           ",
+               "           ",
+               "           "]
+
+loadMaybe "glider" = Just $ parseBoard s
+    where s = ["                   ",
+               "     O             ",
+               "   O O             ",
+               "    OO             ",
+               "                   ",
+               "                   ",
+               "                   ",
+               "                   ",
+               "                   "]
+
+loadMaybe "lwss" = Just $ parseBoard s
+    where s = ["                   ",
+               "  O  O             ",
+               "      O            ",
+               "  O   O            ",
+               "   OOOO            ",
+               "                   ",
+               "                   "]
+
+loadMaybe "r-pent" = Just $ parseBoard s
+    where s = ["                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "            OO            ",
+               "           OO             ",
+               "            O             ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          ",
+               "                          "]
 
 wrap :: Int -> Int -> Board -> (Int, Int)
 wrap x y b = (xr, yr)
@@ -110,6 +230,8 @@ data Command = Quit
              | Next
              | Invalid
              | Help
+             | Presets
+             | Load String
              | Step Int
              | Play Int
              | Place Int Int
@@ -117,41 +239,61 @@ data Command = Quit
              | Resize Int Int
 
 process' :: [String] -> Command
-process' [c, xs, ys] | c == "p" || c == "place"  = f Place  (readMaybe xs) (readMaybe ys)
-                     | c == "t" || c == "toggle" = f Toggle (readMaybe xs) (readMaybe ys)
-                     | c == "r" || c == "resize" = f Resize (readMaybe xs) (readMaybe ys)
+process' [c, a0, a1] |             c == "place"  = f Place  (readMaybe a0) (readMaybe a1)
+                     | c == "t" || c == "toggle" = f Toggle (readMaybe a0) (readMaybe a1)
+                     | c == "r" || c == "resize" = f Resize (readMaybe a0) (readMaybe a1)
     where f g (Just x) (Just y) = g x y
           f _ _        _        = Invalid
-process' [c, xs] | c == "s" || c == "step" = f Step (readMaybe xs)
-                 | c == "p" || c == "play" = f Play (readMaybe xs)
+process' [c, a0] | c == "s" || c == "step" = f Step (readMaybe a0)
+                 | c == "p" || c == "play" = f Play (readMaybe a0)
+                 | c == "l" || c == "load" = Load a0
     where f g (Just n) = g n
           f _ _        = Invalid
 process' _  = Invalid
 
 process :: String -> Command
-process s | s' == "q" || s' == "quit" = Quit
+process s | s' == "q" || s' == "quit"             = Quit
           | s' == "n" || s' == "next" || s' == "" = Next
-          | s' == "h" || s' == "help" = Help
+          | s' == "h" || s' == "help"             = Help
+          |              s' == "presets"          = Presets
           | otherwise = process' $ map trim $ splitOn " " $ trim s'
     where s' = trim s
-
-clearScreenStr :: String
-clearScreenStr = "\ESC[2J"
 
 helpStr :: String
 helpStr = "=-=-= Help =-=-=\n\
           \quit      : Quit Game of Life\n\
           \next      : Next iteraton\n\
           \help      : Print this message\n\
+          \load s    : Loads the preset Game of Life, 's'\n\
+          \presets   : (TODO) Prints a list of presets\n\
           \step n    : Calculate n iteratons, and display the result\n\
           \play n    : Calculate and display n iterations\n\
           \place x y : Create life at the position x,y\n\
           \toggle x t: Toggle the life status of position x,y\n\
           \resize w h: Resize the Game of Life window"
 
+pad :: String -> Int -> Char -> String
+pad s     0 _ = s
+pad []    n x = x : pad [] (n-1) x
+pad (c:s) n x = c : pad s  (n-1) x
+
+presetsStr :: String
+presetsStr = "=-=-= Presets =-=-=\n" ++ (concat $ intersperse "\n" descs)
+    where maxl  = maximum [length x | (x, _) <- presets]
+          descs = [pad s maxl ' ' ++ ": " ++ desc | (s, desc) <- presets]
+
 invalidStr :: String
 invalidStr = "ERROR: Invalid command format.\n\n" ++ helpStr
 
+invalidLoad :: String -> Board -> IO ()
+invalidLoad s b = do putStrLn $ "ERROR: '" ++ s ++ "' is an invalid preset name.\nFor a list of presets, type 'presets'"
+                     prompt b
+
+validLoad :: String -> Board -> IO ()
+validLoad s b = do putStrLn $ "Loaded '" ++ s ++ "'."
+                   play b
+
+-- Executes a command
 playCommand :: Command -> Board -> IO ()
 playCommand Quit         _ = return ()
 playCommand Next         b = play (next b)
@@ -159,6 +301,9 @@ playCommand Invalid      b = do putStrLn invalidStr
                                 prompt b
 playCommand Help         b = do putStrLn helpStr
                                 prompt b
+playCommand Presets      b = do putStrLn presetsStr
+                                prompt b
+playCommand (Load s)     b = maybe (invalidLoad s b) (validLoad s) (loadMaybe s)
 playCommand (Step n)     b = play $ nextN n b
 playCommand (Play n)     b = playN n (next b)
 playCommand (Place  x y) b = play $ setLife x y b Alive
@@ -173,7 +318,7 @@ playN :: Int -> Board -> IO ()
 playN 1 b = play b
 playN n b = do putStrLn $ showBoard b
                b' <- pseq 0 (return (next b))
-               threadDelay $ 100 * millisecond
+               threadDelay $ 150 * millisecond
                playN (n-1) b'
 
 prompt :: Board -> IO ()
@@ -187,5 +332,4 @@ play b = do putStrLn $ showBoard b
             prompt b
 
 main :: IO ()
-main = do putStrLn clearScreenStr
-          play defaultBoard
+main = play defaultBoard
